b5106272531a4adfb8f8b9bf473ad6a1fee4726d
rajarshi-2000 <51237949+rajarshi-2000@users.noreply.github.com>
2021-04-11 16:52:18 +0530

    Add User Login Reducer and Action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,24 +1,32 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux'
-import thunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { categoryListReducer } from './reducers/categoryReducers'
-import { subcategoryListReducer } from './reducers/subcategoryReducers'
-import { productListReducer } from './reducers/productReducers'
+import { createStore, applyMiddleware, combineReducers } from "redux"
+import thunk from "redux-thunk"
+import { composeWithDevTools } from "redux-devtools-extension"
+import { categoryListReducer } from "./reducers/categoryReducers"
+import { subcategoryListReducer } from "./reducers/subcategoryReducers"
+import { productListReducer } from "./reducers/productReducers"
+import { userLoginReducer } from "./reducers/userReducers.js"
 
 const reducer = combineReducers({
-  categoryList: categoryListReducer,
-  subcategoryList: subcategoryListReducer,
-  productList: productListReducer,
+	categoryList: categoryListReducer,
+	subcategoryList: subcategoryListReducer,
+	productList: productListReducer,
+	userLogin: userLoginReducer,
 })
 
-const initialState = {}
+const userInfoFromStorage = localStorage.getItem("userInfo")
+	? JSON.parse(localStorage.getItem("userInfo"))
+	: null
+
+const initialState = {
+	userLogin: { userInfo: userInfoFromStorage },
+}
 
 const middleware = [thunk]
 
 const store = createStore(
-  reducer,
-  initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+	reducer,
+	initialState,
+	composeWithDevTools(applyMiddleware(...middleware))
 )
 
-export default store;
+export default store

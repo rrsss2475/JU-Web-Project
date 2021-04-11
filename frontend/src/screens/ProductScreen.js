4ecf40193a51deb57b40de7d7367225e552906ba
rajarshi-2000 <51237949+rajarshi-2000@users.noreply.github.com>
2021-04-11 16:12:48 +0530

    Fix some formatting problems

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,45 +1,48 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, useParams } from 'react-router-dom';
-import { Row, Col } from 'react-bootstrap';
-import Product from '../components/Product';
-import productList, { listProducts } from '../actions/productActions';
+import React, { useEffect } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { Link, useParams } from "react-router-dom"
+import { Row, Col } from "react-bootstrap"
+import Product from "../components/Product"
+import { listProducts } from "../actions/productActions"
 
 const ProductScreen = () => {
+	const { catName, subCatName } = useParams()
+	const dispatch = useDispatch()
 
-    const { catName, subCatName } = useParams();
-    const dispatch = useDispatch();
+	const productList = useSelector((state) => state.productList)
+	const { loading, error, products } = productList
 
-    const productList = useSelector(state => state.productList);
-    const { loading, error, products } = productList;
+	useEffect(() => {
+		dispatch(listProducts(catName, subCatName))
+	}, [catName, dispatch, subCatName])
 
-    useEffect(() => {
-        dispatch(listProducts(catName, subCatName))
-    }, [dispatch]);
+	let body = <h1></h1>
 
-    let body = (<h1></h1>);
+	if (!loading) {
+		body = (
+			<Row>
+				{products.map((product) => (
+					<Col sm={12} md={6} lg={4} xl={3}>
+						<Product
+							product={product}
+							catName={catName}
+							subCatName={subCatName}
+						/>
+					</Col>
+				))}
+			</Row>
+		)
+	}
 
-    if (loading == false) {
-        body = (
-            <Row>
-                {products.map((product) => (
-                    <Col sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} catName={catName} subCatName={subCatName} />
-                    </Col>
-                ))
-                }
-            </Row>
-        );
-    };
-
-    return (
-        <div className="container" style={{ marginTop: '100px' }}>
-            <Link className='btn btn-dark my-3 mx-2' to={`/categories/${catName}`}>Back to {catName}</Link>
-            <h1>{subCatName}</h1>
-            {loading ? <h1>Loading...</h1> : error ? <h1>Error</h1> : body}
-
-        </div>
-    )
+	return (
+		<div className="container" style={{ marginTop: "100px" }}>
+			<Link className="btn btn-dark my-3 mx-2" to={`/categories/${catName}`}>
+				Back to {catName}
+			</Link>
+			<h1>{subCatName}</h1>
+			{loading ? <h1>Loading...</h1> : error ? <h1>Error</h1> : body}
+		</div>
+	)
 }
 
 export default ProductScreen

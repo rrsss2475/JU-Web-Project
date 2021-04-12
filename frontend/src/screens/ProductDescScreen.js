59d58c41a0244f168e4b10f799e07ad4448e89f7
Reshab Barnwal <reshabbarnwal99@gmail.com>
2021-04-12 16:44:06 +0530

    rating

--- frontend/src/screens/ProductDescScreen.js
+++ frontend/src/screens/ProductDescScreen.js
@@ -1,26 +1,36 @@
 import React from 'react'
-import { Col, Row } from 'react-bootstrap';
+import { Col, Row } from 'react-bootstrap'
+import Rating from '../components/Rating'
 
 const ProductDescScreen = ({ location }) => {
-    const { product } = location.state;
-    //const { product } = state;
-    console.log(product);
-    return (
-        <div className = "container" style={{paddingTop: "75px"}}>
-            <Row>
-            <Col sm={12} md={8} lg={6} xl={6}>
-                <img src={product.image} style={{ height: "100%", width: "100%"}}/>
-            </Col>
-            <Col sm={12} md={8} lg={5} xl={4}>
-                <h1>{product.name}</h1> <br/>
-                <b>Rs {product.price}</b><br/><br/>
-                <p>{product.description}</p> 
-
-            </Col>
-            </Row>
-            
-        </div>
-    )
+  const { product } = location.state
+  //const { product } = state;
+  console.log(product)
+  return (
+    <div className='container' style={{ paddingTop: '75px' }}>
+      <Row>
+        <Col sm={12} md={8} lg={6} xl={6}>
+          <img src={product.image} style={{ height: '100%', width: '100%' }} />
+        </Col>
+        <Col sm={12} md={8} lg={5} xl={4}>
+          <h1>{product.name}</h1>
+          By {product.user}
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
+          <br />
+          <p>
+            <b>Price: Rs {product.price}</b>
+          </p>
+          <br />
+          <br />
+          
+          <p>{product.description}</p>
+        </Col>
+      </Row>
+    </div>
+  )
 }
 
 export default ProductDescScreen

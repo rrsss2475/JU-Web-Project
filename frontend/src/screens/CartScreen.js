6e79b74f84ead043ea6692e4be5c1f2117b2f28d
SAYANDEEP GHOSH <sayandp58@gmail.com>
2021-04-25 16:43:29 +0530

    cart problem solve

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { ListGroup, ListGroupItem, Image } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux'
 import { listCart } from '../actions/cartActions'
 import Loader from '../components/Loader';
@@ -20,34 +20,45 @@ const CartScreen = () => {
 
     useEffect(() => {
         dispatch(listCart(userInfo._id))
-    }, [dispatch, userInfo, cart, cartArr]);
+    }, [dispatch]);
 
-   // const cartArr1 = [];
-    function pushToCartArr(id, qty)
-    {
-        axios.get(`http://localhost:5000/api/products/categories/${id}`)
-        .then(item => setcartArr([...cartArr, item.data]))
-        .catch(err => console.log("error"))
+    useEffect(() => {
+        (async function fun() {
+            for (let i = 0; i < cart.length; i++) {
+                const item = await axios.get(`http://localhost:5000/api/products/categories/${cart[i].product}`)
+                console.log(item.data);
+                setcartArr(prevState => [...prevState, item.data]);
+            }
+        })();
+    }, [cart]);
+
+    /*
+    if (!loading && !error) {
+        useEffect(() => {
+            (async function fun() {
+                for (let i = 0; i < cart.length; i++) {
+                    const item = await axios.get(`http://localhost:5000/api/products/categories/${cart[i].product}`)
+                    console.log(item.data);
+                    setcartArr(prevState=>[...prevState,item.data]);
+                }
+            })();
+        }, [cart]);
     }
-    if(!loading && !error)
-    {
-        for(let i in cart)
-        {
-            pushToCartArr(cart[i].product, cart[i].qty)
-            console.log(cart[i].product)
-        }
-    }
+    */
 
     let body = (
-        <div className="container">
+        <div>
             <ListGroup>
-                {cartArr.map(item => <div><ListGroup.Item>{item.name}</ListGroup.Item><br /></div>)}
+                {cartArr.map(item => <div><ListGroup.Item><Image src={item.image} style={{ height: "150px" }} /></ListGroup.Item><br /></div>)}
             </ListGroup>
         </div>
     );
 
     return (
-        loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : body
+        <div className="container">
+            <h1>Shopping Cart</h1>
+            {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : body}
+        </div>
     )
 }
 

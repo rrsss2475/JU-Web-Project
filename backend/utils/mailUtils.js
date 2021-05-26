ac98133c4353de080ca77126dc1245dfcebaeb09
rajarshi-2000 <51237949+rajarshi-2000@users.noreply.github.com>
2021-05-27 00:31:39 +0530

    route, mail body bug

--- backend/utils/mailUtils.js
+++ backend/utils/mailUtils.js
@@ -1,18 +1,18 @@
-const express = require("express");
-const nodemailer = require("nodemailer");
-const moment = require("moment");
-require("dotenv").config();
+const express = require("express")
+const nodemailer = require("nodemailer")
+const moment = require("moment")
+require("dotenv").config()
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS,
-  },
-});
+	service: "gmail",
+	auth: {
+		user: process.env.GMAIL_USER,
+		pass: process.env.GMAIL_PASS,
+	},
+})
 
 function getMailBody(order, user, type, receipt_url = "") {
-  let body = `<div
+	let body = `<div
   style="padding: 20px; font-family: Rubik, sans-serif"
 >
   <div style="display: flex">
@@ -31,67 +31,67 @@ function getMailBody(order, user, type, receipt_url = "") {
       <b style="font-size: 21px">${type} ${order.status}</b><br />${type} # <a href="http://justintime24-7.herokuapp.com/checkout/${type}/${order._id}">${order._id}</a>
     </p>
   </div>
-  <br />`;
+  <br />`
 
-  /* INITIATED OR SHIPPED */
+	/* INITIATED OR SHIPPED */
 
-  if (order.status == "Initiated" || order.status == "Shipped") {
-    body += `<div style="width: 100%">
+	if (order.status == "Initiated" || order.status == "Shipped") {
+		body += `<div style="width: 100%">
     <h3 style="margin-bottom: -14px; color: green">Hello ${user.name},</h3>
     <p>
-      Thank you for your ${type.toLowerCase()}. Your estimated delivery date is indicated below. If you would
-      like to view the status of your order or make any changes to it,
-      please visit Your Orders on Justintime.
+      Thank you for your ${type.toLowerCase()}. If you would
+      like to view the status of your ${type.toLowerCase()} or make any changes to it,
+      please visit Your ${type}s on Justintime.
     </p>
     </div>
   <hr />
   <div style="display: flex; width: 70%">
-    <div style="width: 100%">`;
-    if (type == "Order") {
-      body += `<p>Arriving:<br /><b style="color: green">${moment(
-        order.toBeDelivered
-      ).format("DD-MM-YYYY")}</b></p>`;
-    } else {
-      body += `<p>Completion on:<br /><b style="color: green">${moment(
-        order.toBeCompleted
-      ).format("DD-MM-YYYY")}</b></p>`;
-    }
-    body += `</div>
+    <div style="width: 100%">`
+		if (type == "Order") {
+			body += `<p>Arriving:<br /><b style="color: green">${moment(
+				order.toBeDelivered
+			).format("DD-MM-YYYY")}</b></p>`
+		} else {
+			body += `<p>Completion on:<br /><b style="color: green">${moment(
+				order.toBeCompleted
+			).format("DD-MM-YYYY")}</b></p>`
+		}
+		body += `</div>
     <div style="width: 100%">
       <p style="float: right">
         Address:<br /><b> ${order.shippingAddress.name},<br />${
-      order.shippingAddress.street
-    },<br />${order.shippingAddress.city}, ${
-      order.shippingAddress.state
-    },<br/>${order.shippingAddress.country}, ${order.shippingAddress.zip}</b>
+			order.shippingAddress.street
+		},<br />${order.shippingAddress.city}, ${
+			order.shippingAddress.state
+		},<br/>${order.shippingAddress.country}, ${order.shippingAddress.zip}</b>
       </p>
     </div>
   </div>
   <button><a style="text-decoration: none" href="http://justintime24-7.herokuapp.com/checkout/${type}/${
-      order._id
-    }">View or manage ${type.toLowerCase()}</a></button>
+			order._id
+		}">View or manage ${type.toLowerCase()}</a></button>
   <div>
     <h3 style="color: green">${type} Summary</h3>
     <hr style="margin-top: -14px; margin-bottom: -14px" />
     <p>${type} # <a href="http://justintime24-7.herokuapp.com/checkout/${type}/${
-      order._id
-    }">${order._id}</a><br />Placed on ${moment(order.createdAt).format(
-      "DD-MM-YYYY"
-    )}</p>
+			order._id
+		}">${order._id}</a><br />Placed on ${moment(order.createdAt).format(
+			"DD-MM-YYYY"
+		)}</p>
     <table>
       <tr>
         <th></th>
         <th></th>
         <th></th>
-      </tr>`;
-    if (type == "Order") {
-      body += `<tr>
+      </tr>`
+		if (type == "Order") {
+			body += `<tr>
         <td>Total Items</td>
         <td>&emsp;</td>
         <td>${order.orderItems.length}</td>
-      </tr>`;
-    }
-    body += `<tr></tr>
+      </tr>`
+		}
+		body += `<tr></tr>
       <tr></tr>
       <tr>
         <td><b>Total Price</b></td>
@@ -101,19 +101,19 @@ function getMailBody(order, user, type, receipt_url = "") {
     </table>
   </div>
   <hr />
-  <div>`;
-    if (order.isPaid) {
-      body += `<p>
+  <div>`
+		if (order.isPaid) {
+			body += `<p>
       Payment made using ${order.paymentMethod}<br />Check your receipt
       <a href="${receipt_url}">here</a>
-    </p>`;
-    }
-  }
+    </p>`
+		}
+	}
 
-  /* DELIVERED */
+	/* DELIVERED */
 
-  if (order.status == "Delivered" || order.status == "Completed") {
-    body += `<div style="width: 100%">
+	if (order.status == "Delivered" || order.status == "Completed") {
+		body += `<div style="width: 100%">
     <h3 style="margin-bottom: -14px; color: green">Hello ${user.name},</h3>
     <p>
       Your ${type.toLowerCase()} has been ${order.status.toLowerCase()}.
@@ -121,52 +121,52 @@ function getMailBody(order, user, type, receipt_url = "") {
     </div>
   <hr />
   <div style="display: flex; width: 70%">
-    <div style="width: 100%">`;
-    if (type == "Order") {
-      body += `<p>Delivered on:<br /><b style="color: green">${moment(
-        order.deliveredAt
-      ).format("DD-MM-YYYY")}</b></p>`;
-    } else {
-      body += `<p>Completed on:<br /><b style="color: green">${moment(
-        order.completedAt
-      ).format("DD-MM-YYYY")}</b></p>`;
-    }
-    body += `</div>
+    <div style="width: 100%">`
+		if (type == "Order") {
+			body += `<p>Delivered on:<br /><b style="color: green">${moment(
+				order.deliveredAt
+			).format("DD-MM-YYYY")}</b></p>`
+		} else {
+			body += `<p>Completed on:<br /><b style="color: green">${moment(
+				order.completedAt
+			).format("DD-MM-YYYY")}</b></p>`
+		}
+		body += `</div>
     <div style="width: 100%">
       <p style="float: right">
         Address:<br /><b> ${order.shippingAddress.name},<br />${
-      order.shippingAddress.street
-    },<br />${order.shippingAddress.city}, ${
-      order.shippingAddress.state
-    },<br/>${order.shippingAddress.country}, ${order.shippingAddress.zip}</b>
+			order.shippingAddress.street
+		},<br />${order.shippingAddress.city}, ${
+			order.shippingAddress.state
+		},<br/>${order.shippingAddress.country}, ${order.shippingAddress.zip}</b>
       </p>
     </div>
   </div>
   <button><a style="text-decoration: none" href="http://justintime24-7.herokuapp.com/checkout/${type}/${
-      order._id
-    }">View or manage ${type.toLowerCase()}</a></button>
+			order._id
+		}">View or manage ${type.toLowerCase()}</a></button>
   <div>
     <h3 style="color: green">${type} Summary</h3>
     <hr style="margin-top: -14px; margin-bottom: -14px" />
     <p>${type} # <a href="http://justintime24-7.herokuapp.com/checkout/${type}/${
-      order._id
-    }">${order._id}</a><br />Placed on ${moment(order.createdAt).format(
-      "DD-MM-YYYY"
-    )}</p>
+			order._id
+		}">${order._id}</a><br />Placed on ${moment(order.createdAt).format(
+			"DD-MM-YYYY"
+		)}</p>
     <table>
       <tr>
         <th></th>
         <th></th>
         <th></th>
-      </tr>`;
-    if (type == "Order") {
-      body += `<tr>
+      </tr>`
+		if (type == "Order") {
+			body += `<tr>
         <td>Total Items</td>
         <td>&emsp;</td>
         <td>${order.orderItems.length}</td>
-      </tr>`;
-    }
-    body += `<tr></tr>
+      </tr>`
+		}
+		body += `<tr></tr>
       <tr></tr>
       <tr>
         <td><b>Total Price</b></td>
@@ -176,98 +176,43 @@ function getMailBody(order, user, type, receipt_url = "") {
     </table>
   </div>
   <hr />
-  <div>`;
-    if (order.isPaid) {
-      body += `<p>
+  <div>`
+		if (order.isPaid) {
+			body += `<p>
       Payment made using ${order.paymentMethod}<br />Check your receipt
       <a href="${receipt_url}">here</a>
-    </p>`;
-    }
-  }
+    </p>`
+		}
+	}
 
-  /* CANCELLED */
+	/* CANCELLED */
 
-  if (order.status == "Cancelled") {
-    body += `<div style="width: 100%">
+	if (order.status == "Cancelled") {
+		body += `<div style="width: 100%">
       <h3 style="margin-bottom: -14px; color: green">Hello ${user.name},</h3><p>
-      Your ${type.toLowerCase()} has been <b>cancelled</b>. You can view the status of your order from below button.
+      Your ${type.toLowerCase()} has been <b>cancelled</b>. You can view the status of your ${type.toLowerCase()} from below button.
     </p>
     </div>
   <button><a style="text-decoration: none" href="http://justintime24-7.herokuapp.com/checkout/${type}/${
-      order._id
-    }">View or manage ${type.toLowerCase()}</a></button>
-  <hr/>`;
-    if (order.isPaid) {
-      body += `<p>
+			order._id
+		}">View or manage ${type.toLowerCase()}</a></button>
+  <hr/>`
+		if (order.isPaid) {
+			body += `<p>
     Payment made using ${order.paymentMethod}<br />
     Refund will be initiated in 2-3 business days
-  </p>`;
-    }
-  }
+  </p>`
+		}
+	}
 
-  body += `<p>We hope to see you again soon.</p>
+	body += `<p>We hope to see you again soon.</p>
     <h2 style="margin-top: -16px">Justintime</h2>
   </div>
-</div>`;
-
-  // let body = "<div><h3>" + type + " " + order._id + "</h3>";
-  // body +=
-  //   "<a href=" +
-  //   `http://localhost:3000/checkout/order/${order._id}` +
-  //   `>Track your ${type}</a>`;
-
-  // body += `<h4>Your ${type} has been ${order.status}</h4>`;
-
-  // if (order.status == "Initiated" || order.status == "Shipped") {
-  //   if (type == "Order") {
-  //     body += `<p>Delivery Date: ${moment(order.toBeDelivered).format(
-  //       "DD-MM-YYYY"
-  //     )}</p>`;
-  //   } else {
-  //     body += `<p>Completion Date: ${moment(order.toBeCompleted).format(
-  //       "DD-MM-YYYY"
-  //     )}</p>`;
-  //   }
-  // } else if (order.status == "Delivered" || order.status == "Completed") {
-  //   if (type == "Order") {
-  //     body += `<p>Delivery Date: ${moment(order.deliveredAt).format(
-  //       "DD-MM-YYYY"
-  //     )}</p>`;
-  //   } else {
-  //     body += `<p>Completion Date: ${moment(order.completedAt).format(
-  //       "DD-MM-YYYY"
-  //     )}</p>`;
-  //   }
-  // }
-
-  // if (order.status != "Cancelled") {
-  //   body += "<ul>";
-  //   if (type == "Order") {
-  //     for (const item of order.orderItems) {
-  //       body += `<div style="display:flex;"><img style="height:50px; width:50px;" src="${item.image}" alt="${item.name}"></img><p>&nbsp;&nbsp;&nbsp;&nbsp;${item.name}</p></div>`;
-  //     }
-  //   } else {
-  //     body += `<div style="display:flex;"><img style="height:50px; width:50px;" src="${order.bookingItem.image}" alt="${order.bookingItem.name}"></img><p>&nbsp;&nbsp;&nbsp;&nbsp;${order.bookingItem.name}</p></div>`;
-  //   }
-  //   body += "</ul>";
-  //   body += `<p>Total Amount : ₹ ${order.totalPrice}</p>`;
-  //   body += `<p>Address : ${order.shippingAddress.name}, ${order.shippingAddress.street}, ${order.shippingAddress.city}, ${order.shippingAddress.state}, ${order.shippingAddress.country}, ${order.shippingAddress.zip}</p>`;
-  //   if (order.isPaid) {
-  //     body += `<p>Payment made using ${order.paymentMethod}</p>`;
-  //     if (order.status == "Initiated") {
-  //       body += `<p>Check your receipt <a href="${receipt_url}">here</a></p>`;
-  //     }
-  //   }
-  // } else {
-  //   if (order.isPaid) {
-  //     body += `<p>Refund will be initiated in 2-3 business days</p>`;
-  //   }
-  // }
-  // body += "</div>";
-  return body;
+</div>`
+	return body
 }
 
 module.exports = {
-  transporter: transporter,
-  getMailBody: getMailBody,
-};
+	transporter: transporter,
+	getMailBody: getMailBody,
+}
